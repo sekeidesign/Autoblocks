c2bc69d57664380c6d256acd8155197477c9fe96
Pg Gonni <59372630+pggonni@users.noreply.github.com>
2021-03-03 20:29:51 -0500

    UI 1 - Commit 1.1
    ✅ Create basic Vue component to test functionality
    ✅ Create a button
    
    Also
    ✅ Add basic styles
    ✅ Add CSS reset

--- webApp/src/App.vue
+++ webApp/src/App.vue
@@ -1,26 +1,136 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld/>
+  <APItest/>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import APItest from "./components/APItest.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    APItest
   },
 };
 </script>
 
 <style>
+/* --------- CSS Reset --------- */
+
+/* Box sizing rules */
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+/* Remove default margin */
+body,
+h1,
+h2,
+h3,
+h4,
+p,
+figure,
+blockquote,
+dl,
+dd {
+  margin: 0;
+}
+
+/* Remove list styles on ul, ol elements with a list role, which suggests default styling will be removed */
+ul[role='list'],
+ol[role='list'] {
+  list-style: none;
+}
+
+/* Set core root defaults */
+html:focus-within {
+  scroll-behavior: smooth;
+}
+
+/* Set core body defaults */
+body {
+  min-height: 100vh;
+  text-rendering: optimizeSpeed;
+  line-height: 1.5;
+}
+
+/* A elements that don't have a class get default styles */
+a:not([class]) {
+  text-decoration-skip-ink: auto;
+}
+
+button {
+  border: none;
+  outline: none;
+}
+
+/* Make images easier to work with */
+img,
+picture {
+  max-width: 100%;
+  display: block;
+}
+
+/* Inherit fonts for inputs and buttons */
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+
+/* Remove all animations, transitions and smooth scroll for people that prefer not to see them */
+@media (prefers-reduced-motion: reduce) {
+  html:focus-within {
+   scroll-behavior: auto;
+  }
+  
+  *,
+  *::before,
+  *::after {
+    animation-duration: 0.01ms !important;
+    animation-iteration-count: 1 !important;
+    transition-duration: 0.01ms !important;
+    scroll-behavior: auto !important;
+  }
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #37352F;
 }
+
+/* --------- Colour Variables --------- */
+
+:root{
+  --blue-600: rgba(46, 170, 220, 1);
+  --blue-500: rgba(6, 156, 205, 1);
+  --blue-400: rgba(0, 145, 190, 1);
+}
+
+.btn-primary {
+  height: 32px;
+  padding: 0px 12px;
+  box-shadow: 0px 1px 2px rgba(15, 15, 15, 0.1), inset 0px 0px 1px rgba(15, 15, 15, 0.5);
+  border-radius: 3px;
+  text-align: center;
+  background-color: var(--blue-600);
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  font-size: 14px;
+  cursor: pointer;
+}
+.btn-primary:hover {
+  background-color: var(--blue-500);
+}
+.btn-primary:active {
+  background-color: var(--blue-400);
+}
+
 </style>
---
+++ webApp/src/components/APItest.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="wrap">
+        <div class="test-card">
+            <h1>Test me out, baby!</h1>
+            <p>Check me out, try the thing. <br> I’m a test to see if you suck at dev (spoilers, you do)</p>
+            <button @click="apiTest" class="btn-primary">Make an API call</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "APItest",
+    methods: {
+        apiTest: function () {
+            fetch("http://localhost:3000/json_placeholder/databases/11af47c3d89c4afd9000518e7ff90d03/query", {
+                headers: {
+                    Authorization: "Bearer secret_xOLs7UclNIojYcjgJVPfco5zMciCRny5a30FNofTGxK",
+                    "Content-Type": "application/json"
+                },
+                method: "POST"
+            })
+        },
+    },
+}
+</script>
+
+<style scoped>
+    .wrap {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0px 16px;
+    }
+    .test-card {
+        padding: 40px 56px;
+        border-radius: 3px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background: white;
+        box-shadow: 0px 4px 12px rgba(55, 53, 47, 0.1);
+        text-align: center;
+    }
+    h1 {
+        margin-bottom: 8px;
+    }
+    p {
+        margin-bottom: 24px;
+    }
+</style>
